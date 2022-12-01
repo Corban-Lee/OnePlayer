148228fd1b8697796e5f73abdbbdcab283fd13a2
Corban-Lee <77944149+XordK@users.noreply.github.com>
2022-12-01 19:00:08 +0000

    init commit

---
+++ src/bot/__init__.py
@@ -0,0 +1,5 @@
+"""Bot file"""
+
+
+class Bot:
+    pass
---
+++ src/main.py
@@ -0,0 +1,10 @@
+"""Entry point for the application script"""
+
+from bot import Bot
+
+def main():
+    """Main entry point for the application"""
+    print("Call your main application code here")
+
+if __name__ == "__main__":
+    main()
